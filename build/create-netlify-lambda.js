4183f8281b4e0dcbe5d590e11e9ad0ae190bcdd7
AWolf81 <awolf2904@gmail.com>
2018-05-07 07:36:23 +0200

    removed join

--- build/create-netlify-lambda.js
+++ build/create-netlify-lambda.js
@@ -2,6 +2,7 @@ const childProcess = require('child_process')
 const path = require('path');
 
 module.exports = function() {
-    const srcPath = path.join(__dirname, '../src/lambda')
+    const srcPath = '../src/lambda'
+    childProcess.execSync(`ls -lah ${srcPath}`) // just to see the directory
     childProcess.execSync(`netlify-lambda build ${srcPath}`)
 }
